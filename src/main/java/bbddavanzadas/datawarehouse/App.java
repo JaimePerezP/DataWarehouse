5ecfe56bc0bc046b0b69bac829c4f8da2938bf38
jorgemm7 <jorgemm777@gmail.com>
2020-02-24 18:49:15 +0100

    Create Tables
    Se crean las tablas con la estructura que quieres que tenga el Data Warehouse

--- src/main/java/bbddavanzadas/datawarehouse/App.java
+++ src/main/java/bbddavanzadas/datawarehouse/App.java
@@ -11,6 +11,7 @@ public class App {
 
 	public static void main(String[] args) {
 		Connection con = DBBroker.connect();
+		DBBroker.createTables();
 	}
 
 	public static void parsear() {
--- src/main/java/bbddavanzadas/datawarehouse/DBBroker.java
+++ src/main/java/bbddavanzadas/datawarehouse/DBBroker.java
@@ -3,22 +3,31 @@ package bbddavanzadas.datawarehouse;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DBBroker {
 
 	public static Connection connect() {
 		Connection con = null;
 		String driver = "com.mysql.cj.jdbc.Driver";
-		String database = "datawarehouse-youtube"; // Hay que crear la DB
+//		Database de Jaime
+//		String database = "datawarehouse-youtube";
+//		Database de Jorge
+		String database = "datawarehouse"; // Hay que crear la DB
 		String hostname = "127.0.0.1";
 		String port = "3306";
-		String url = "jdbc:mysql://" + hostname + ":" + port + "/" + database + "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+		String url = "jdbc:mysql://" + hostname + ":" + port + "/" + database
+				+ "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 		String username = "root";
-		String password = "mysql";
+//		Contraseña de Jaime
+//		String password = "mysql";
+//		Contraseña de Jorge
+		String password = "jorgejaime123";
 
 		try {
 			Class.forName(driver);
 			con = DriverManager.getConnection(url, username, password);
+			System.out.println("DB connected");
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
@@ -26,4 +35,20 @@ public class DBBroker {
 		return con;
 	}
 
+	public static void createTablesMySQL(String sql) {
+		System.out.println(sql);
+		try (Connection conn = connect(); Statement stmt = conn.createStatement()) {
+			stmt.execute(sql);
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public static void createTables() {
+		createTablesMySQL("CREATE TABLE IF NOT EXISTS Trending_Video_Youtube (\n id_trending_video_yt integer PRIMARY KEY,\n id_dates integer NOT NULL,\n id_options integer NOT NULL,\n id_group integer NOT NULL,\n id_statistics integer NOT NULL,\n title varchar(255) NOT NULL,\n channel_title varchar(255) NOT NULL,\n description varchar(255) NOT NULL,\n thumbnail_link varchar(255) NOT NULL\n);\n");
+		createTablesMySQL("CREATE TABLE IF NOT EXISTS Video_Date (\n id_dates integer PRIMARY KEY,\n trending_date date NOT NULL,\n publish_time date NOT NULL\n);\n");
+		createTablesMySQL("CREATE TABLE IF NOT EXISTS Video_Options (\n id_options integer PRIMARY KEY,\n retings_disabled varchar(255) NOT NULL,\n comments_disabled varchar(255) NOT NULL,\n video_error_or_removed varchar(255) NOT NULL\n);\n");
+		createTablesMySQL("CREATE TABLE IF NOT EXISTS Video_Group (\n id_group integer PRIMARY KEY,\n category_id integer NOT NULL,\n tags varchar(255) NOT NULL\n);\n");
+		createTablesMySQL("CREATE TABLE IF NOT EXISTS Video_Statistics (\n id_statistics integer PRIMARY KEY,\n views integer NOT NULL,\n likes integer NOT NULL,\n dislike integer NOT NULL,\n comments_count varchar(255) NOT NULL \n);\n");
+	}
 }
